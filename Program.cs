e94398571da1504f616cfdf9906310b0d27a2aa0
Petr Starostin <starostin.petr@yandex.ru>
2023-03-10 22:21:23 +0400

    Сделаны 3 задачи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// int GetMaxNumber(int num)
+// {
+//     int num1 = num /10;
+//     int num2 =  num % 10;
+//     if(num1 < num2) 
+//             return num2;
+//     else return num1 ;
+// }
+// int randNumber = new Random().Next(10, 100);
+// Console.WriteLine($"Input nummer {randNumber} max number {GetMaxNumber(randNumber)}");
+
+//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно a и b.(bool)
+// bool Kratnoe(int num , int div1 , int div2)
+// {
+//     if(num % div1 == 0 && num % div2 == 0)
+//     {
+//         return true;
+//     }
+
+//     return false;
+// }
+// Console.Write("Введите число ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первый делитель ");
+// int div1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второй делитель ");
+// int div2 = Convert.ToInt32(Console.ReadLine());
+// bool result = Kratnoe(num , div1, div2);
+// Console.WriteLine($"Результат: {result}");
+
+//Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.(bool)
+// bool SquareNumber(int num1 , int num2)
+// {
+//     if(num1 == num2 * num2 || num2 == num1 * num1)
+//     {
+//         return true; 
+//     }
+//     return false;
+// }
+// Console.Write("Введите первое число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// bool result = SquareNumber(num1, num2);
+// Console.WriteLine($"Результат работы: {result}");
