7498d91209dafd31b7a9a940089560e5b8a1296f
Petr Starostin <starostin.petr@yandex.ru>
2023-03-14 17:47:30 +0400

    First taskе

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int SecondNumber(int num)
+{
+int result = num / 10 % 10 ;
+return result ;
+}
+int number =  new Random().Next(100,1000);
+Console.WriteLine($"Число: {number}");
+Console.WriteLine($"Второе число: {SecondNumber(number)}");
+
+
